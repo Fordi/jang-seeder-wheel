3e0a4bbd49578e1b559c91778e68766f8e58b4e6
Bryan Elliott <fordiman@gmail.com>
2023-06-29 14:52:46 -0400

    * Moved CPU detection message into `all` handler * Hoisted preset load into main render function; used it to detect if a preset doesn't exist (see Issue #8) * Added usage message

--- render.sh
+++ render.sh
@@ -4,8 +4,6 @@ PRESETS="$(dirname "$0")/seed_roller.json"
 RENDER_DIR="$(dirname "$0")/stl"
 MAX_THREADS=$(lscpu -p | grep -v '#' | wc -l)
 
-echo "Detected ${MAX_THREADS} CPUs"
-
 OPENSCAD="$(which openscad)"
 # Need to update the scad to support ImplicitCAD,
 # which is a _much_ faster renderer.
@@ -30,11 +28,16 @@ function fJQ() {
 function render() {
   local type=$1; shift;
   local out_dir=$1; shift;
+  local preset=(
+    $(fJQ -r '.parameterSets["'"$type"'"] | to_entries | .[] | "-D" + .key + "=" + (.value | @json)' "$PRESETS" 2>/dev/null)
+  )
+  if [[ -z "$preset" ]]; then
+    echo "No preset named '$type'" >&2
+    exit -1
+  fi
+  echo "Rendering $type"
   if [[ -x "$IMPLICITCAD" ]]; then # 
-    # ImplicitCAD doesn't support presets; transform the preset to variable declarations
-    local preset=(
-      $(fJQ -r '.parameterSets["'"$type"'"] | to_entries | .[] | "-D" + .key + "=" + (.value | @json)' "$PRESETS")
-    )
+    # ImplicitCAD doesn't support presets; transform the preset to variable declarations 
     set -x
     "$IMPLICITCAD" -o "$out_dir/$type.stl" ${preset[@]} "$ENTRY"
     set +x
@@ -51,8 +54,8 @@ function list_presets() {
   fJQ -r '.parameterSets | keys[]' "$PRESETS"
 }
 
-
 if [[ "$1" == "all" ]]; then
+  echo "Detected ${MAX_THREADS} CPUs"
   mkdir -p "$RENDER_DIR"
   OPEN_PIDS=()
   OPEN_RENDERS=()
@@ -86,10 +89,15 @@ if [[ "$1" == "all" ]]; then
   wait
 elif [[ "$1" == "list" ]]; then
   list_presets
-else
+elif [[ "$1" != "" ]]; then
   while [[ "$1" != "" ]]; do
-    echo "Rendering $1"
     render "$1" "./stl"
     shift
   done
+else
+  echo 'Usage: '"$0"' [all|list|"preset"]' >&2
+  echo '    list: List all presets' >&2
+  echo '"preset": Render one preset' >&2
+  echo '     all: Render all presets (takes some time)' >&2
+  exit
 fi
